63fa8772b38e3c47bd733c70a6e4da607e33d551
Zenrool <lorenzopapperini@hotmail.it>
2022-05-19 17:03:30 +0200

    Aggiunto main senza elementi con bootsrap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <SpotiHeader />
+    <SpotiMain />
   </div>
 </template>
 
 <script>
 import SpotiHeader from "./components/SpotiHeader.vue";
+import SpotiMain from "./components/SpotiMain.vue";
 export default {
   name: "App",
   components: {
     SpotiHeader,
+    SpotiMain,
   },
 };
 </script>
---
+++ src/components/SpotiMain.vue
@@ -0,0 +1,23 @@
+<template>
+  <main
+    class="main container-fluid d-flex align-items-center justify-content-center"
+  >
+    <div class="container"></div>
+  </main>
+</template>
+<script>
+export default {
+  name: "SpotiMain",
+};
+</script>
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+.main {
+  height: calc(100vh - 90px);
+  background-color: $brand-primary-color;
+  .container {
+    background-color: red;
+    height: 100px;
+  }
+}
+</style>
