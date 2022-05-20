da13a5989a8f12ecfb92cf6724af98e5d55ca252
Zenrool <lorenzopapperini@hotmail.it>
2022-05-20 14:54:20 +0200

    Select senza evento

---
+++ src/components/SearchGenre.vue
@@ -0,0 +1,25 @@
+<template>
+  <select name="genre" id="genre">
+    <option value="all">All</option>
+    <option value="Pop">Pop</option>
+    <option value="Rock">Rock</option>
+    <option value="Metal">Metal</option>
+    <option value="Jazz">Jazz</option>
+  </select>
+</template>
+<script>
+export default {
+  name: "SearchGenre",
+};
+</script>
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+select {
+  width: 80%;
+  font-size: 1.6rem;
+  background-color: $brand-primary-color;
+  color: white;
+  border-color: lime;
+  border-radius: 5px;
+}
+</style>
--- src/components/SpotiHeader.vue
+++ src/components/SpotiHeader.vue
@@ -4,12 +4,19 @@
       <div class="offset-1 col-1 h-75">
         <img src="../assets/img/logo.png" alt="logo" />
       </div>
+      <div class="offset-7 col-2">
+        <SearchGenre />
+      </div>
     </div>
   </header>
 </template>
 <script>
+import SearchGenre from "./SearchGenre.vue";
 export default {
   name: "SpotiHeader",
+  components: {
+    SearchGenre,
+  },
 };
 </script>
 <style lang="scss" scoped>
